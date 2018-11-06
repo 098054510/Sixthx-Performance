f329dcd4f2195989d86fa1e8183dee0cf007b35a
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 07:51:46 -0300

    Sixth Performance/Program made in C#

---
+++ SixthPerformance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace SixthPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Glic;
+
+            Glic = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (Glic <= 100.00){
+                Console.WriteLine("Normal");
+
+            }
+            else if (Glic <= 140.0){
+                Console.WriteLine("Nivel de Glicose Elevado.");
+            }
+
+            else {
+                Console.WriteLine("Mano, Tu tá com Diabetes, Fera.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
